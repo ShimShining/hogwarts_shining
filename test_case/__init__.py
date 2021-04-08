ca2ade49b0f5e2eb23ac2a9dc2671607784b1752
JoltShining <shinjolt@gmail.com>
2021-04-08 20:26:51 +0800

    FIRST commit

---
+++ test_case/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python37
+# _*_ coding:utf-8 _*_
+"""
+Author : 'Shining'
+Date:
+Describe:
+"""
---
+++ test_case/test_sample.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python37
+# _*_ coding:utf-8 _*_
+"""
+Author : 'Shining'
+Date:
+Describe:
+"""
+
+# content of test_sample.py
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
